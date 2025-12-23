d2b212df9004f58f5657e17f76949095783d8d8d
Sakib Fakir <FakirSakib22232@gmail.com>
2025-12-24 01:14:36 +0600

    feat(prompt) add zod validation

--- app/src/modules/prompt/prompt.controller.ts
+++ app/src/modules/prompt/prompt.controller.ts
@@ -5,11 +5,10 @@ import { NextFunction, Request, Response } from 'express';
 import { Prompt } from './prompt.model';
 import { ReturnResponse } from '../../helper/ReturnResponse';
 
-
 const storage = multer.memoryStorage();
 export const upload = multer({ storage: storage });
 
-// db desigin -> real test
+//zod -> swagger > db desigin -> real test
 
 const promptImageUpload = async (
   req: Request,
@@ -39,76 +38,80 @@ const promptImageUpload = async (
   }
 };
 
-// 
-// add zod validation 
-const createPrompt = async (req:Request, res:Response,next:NextFunction)=>{
-
-
+//
+// add zod validation
+const createPrompt = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     // title,image,tags,profile,prompt
 
     const promptData = req.body;
     const result = await Prompt.create(promptData);
 
-    return ReturnResponse(res, 201, true, 'prompt create successfull',result);
-    
-
-
-
-    
+    return ReturnResponse(res, 201, true, 'prompt create successfull', result);
   } catch (error) {
     next(error);
-    
   }
-
- 
-
-}
+};
 
 // Edit prompt
-// add validation 
-const updatePrompt = async (req:Request, res:Response, next:NextFunction)=>{
-
-
+// add validation
+const updatePrompt = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
-
     const userId = req?.user?.id;
 
-    const data = req.body;
-    const {image , title , tags, prompt} = data ;
-    const payload = {
-      title:title,
-      prompt:prompt,
-      tags:tags,
-      image:image
+    if (!userId) {
+      return ReturnResponse(res, 401, false, 'User unauthorize');
     }
-  
 
-    const result = await Prompt.findByIdAndUpdate(userId,payload,{
-      new:true, upsert:false
-    })
+    const data = req.body;
+    const { image, title, tags, prompt } = data;
+    const payload = {
+      title: title,
+      prompt: prompt,
+      tags: tags,
+      image: image,
+    };
+
+    const result = await Prompt.findByIdAndUpdate(userId, payload, {
+      new: true,
+      upsert: false,
+    });
+
+    if (!result) {
+      return ReturnResponse(res, 404, false, 'Not find any data with this id');
+    }
     // new return updated document
 
     return ReturnResponse(res, 200, true, 'prompt update successfully', result);
-
-    
-
-  
-    
   } catch (error) {
-  next(error);
-    
-    
+    next(error);
   }
-}
+};
 
 // delete prompt
 
-const deletePrompt = async (req: Request, res: Response, next: NextFunction) => {
+const deletePrompt = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const userId = req?.user?.id;
     if (!userId) {
-      return ReturnResponse(res, 401, false, 'User authentication token missing');
+      return ReturnResponse(
+        res,
+        401,
+        false,
+        'User authentication token missing',
+      );
     }
 
     const deleted = await Prompt.findOneAndDelete({ user: userId });
@@ -124,5 +127,8 @@ const deletePrompt = async (req: Request, res: Response, next: NextFunction) => 
 };
 
 export const promptController = {
-  promptImageUpload,createPrompt,updatePrompt,deletePrompt
+  promptImageUpload,
+  createPrompt,
+  updatePrompt,
+  deletePrompt,
 };
---
+++ app/src/modules/prompt/prompt.validation.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+
+
+const createPromptSchema = z.object({
+  title: z.string().min(3).max(100),
+  prompt: z.string().min(10),
+  tags: z.array(z.string()).optional().default([]),
+  image: z.string().url().optional()
+ 
+});
+
+const updatePromptSchema = z.object({
+  title: z.string().min(3).max(100).optional(),
+  prompt: z.string().min(10).optional(),
+  tags: z.array(z.string()).optional(),
+  image: z.string().url().optional(),
+});
+
+export const zodValidationPrompt = {
+    createPromptSchema, updatePromptSchema
+}
