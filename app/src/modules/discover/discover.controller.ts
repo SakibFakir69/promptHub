40dc2409c94b0f39d4b8afe3281f86b3b953f63d
Sakib Fakir <FakirSakib22232@gmail.com>
2026-04-30 01:53:07 +0800

    feat(discover) add discover people api

--- app/src/modules/discover/discover.controller.ts
+++ app/src/modules/discover/discover.controller.ts
@@ -1,11 +1,68 @@
+import { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import { User } from '../users/user.model';
 
-// index, perfomence , query filter , ect + ( learning + imp)
+export const discoverPeople = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+
+    const cursor = req.query.cursor as string | undefined;
+    const limit = parseInt(req.query.limit as string) || 10;
+    const name = req.query.name as string | undefined;
+    const gender = req.query.gender as string | undefined;
+    const age = req.query.age as string | undefined;
 
 
-const discoverPeople = ()=>{}
+    const query: any = {};
 
 
-export const discoverController ={
+    if (name) {
+      query.$or = [
+        { name: { $regex: name, $options: 'i' } },
+        { email: { $regex: name, $options: 'i' } }
+      ];
+    }
+
+    if (gender) {
+      query.gender = gender;
+    }
+
+    if (age) {
+      query.age = parseInt(age);
+    }
+
+    
+    if (cursor && mongoose.Types.ObjectId.isValid(cursor)) {
+      query._id = { $lt: new mongoose.Types.ObjectId(cursor) };
+    }
+
+    
+    const userData = await User.find(query) 
+      .select(
+        'name gender bio avatar totalPost followers following isVerify createdAt age'
+      )
+      .sort({ _id: -1 }) 
+      .limit(limit)
+      .lean();
+
+    
+    const nextCursor =
+      userData.length === limit ? userData[userData.length - 1]._id : null;
+
+    return res.status(200).json({
+      success: true,
+      data: userData,
+      nextCursor,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const discoverController={
     discoverPeople
-
 }
