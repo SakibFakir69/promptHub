4af6dbafc0bcc9728a777a5b9aa73043791515d6
Sakib Fakir <FakirSakib22232@gmail.com>
2026-06-22 00:47:05 +0800

    feat(config) firebase config issue fix

--- app/src/config/firebase/firebase.ts
+++ app/src/config/firebase/firebase.ts
@@ -1,11 +1,12 @@
-import admin from 'firebase-admin';
+import { initializeApp, cert } from "firebase-admin/app";
+import { getMessaging } from "firebase-admin/messaging";
 
-admin.initializeApp({
-  credential: admin.credential.cert({
+const app = initializeApp({
+  credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   }),
 });
 
-export const fcmMessaging  = admin.messaging();
+export const fcmMessaging = getMessaging(app);
