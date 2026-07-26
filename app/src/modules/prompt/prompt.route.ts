7953c96f0bdd47a6c8e231bde0e6768d9f16e4ec
Sakib Fakir <FakirSakib22232@gmail.com>
2026-07-26 21:34:10 +0800

    fix(prompt) route nameing issue

--- app/src/modules/prompt/prompt.route.ts
+++ app/src/modules/prompt/prompt.route.ts
@@ -294,7 +294,7 @@ router.delete('/delete-prompt/:id',verifyToken, promptController.deletePrompt);
  *                   example: "Internal server error"
  */
 
-router.get('/get-prompt',verifyToken, promptController.getAllPrompt);
+router.get('/get-prompt',verifyToken, promptController.getAllPrompts);
 /**
  * @swagger
  * /upVote:
